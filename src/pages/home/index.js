bcaf21081d6e3f475531fc701c221a93e738ff71
jtam2204 <jtam2204@gmail.com>
2023-02-26 16:56:02 +0800

    addin images

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,35 +1,57 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import {SearchBar } from "@rneui/themed";
+import { StyleSheet, Image, Text, View, FlatList } from "react-native";
+import {SearchBar } from "@rneui/themed"; // npm this package for search bar functions
 
 const DATA = [
   {
     id: "1",
+    groupimage: '../../res/christmasparty.jpg',
     title: "Christmas Party",
     description: "ABC",
+    grpprogress: 60, 
   },
   {
     id: "2",
+    groupimage: '../../res/gradtrip.jpg',
     title: "Grad trip",
     description: "ABC",
+    grpprogress: 65,
   },
   {
     id: "3",
+    groupimage: '../../res/igshop.jpg',
     title: "New shop",
     description: "ABC",
+    grpprogress: 35,
   },
 ];
-
-const Item = ({title, description}) => {
+ // problem in displaying image
+const Item = ({groupimage, title, description, grpprogress}) => {
   return (
     <View style={styles.item}>
-      <Text>Group name: {title}</Text>
-      <Text>Description: {description}</Text>
+      <Image source={groupimage} style={styles.image}/> 
+      <View style={styles.grpinfo}>
+        <View style={styles.row}>
+          <Text numberOfLines={1} style={styles.name}>
+          {title}
+          </Text>
+          <Text>
+          Group progress: {grpprogress}% 
+          </Text>
+        </View>
+      <Text numberOfLines={2} style={styles.subtitle}>
+      Description: {description}
+      </Text>
+      </View>
     </View>
   );
 };
 
-const renderItem = ({ item }) => <Item title={item.title} description={item.description} />;
+const renderItem = ({ item }) => <Item 
+groupimage={item.groupimage} 
+title={item.title} 
+description={item.description} 
+grpprogress={item.grpprogress} />;
 class Search extends Component {
   constructor(props) {
     super(props);
@@ -75,15 +97,48 @@ class Search extends Component {
 export default Search;
 
 const styles = StyleSheet.create({
+  image:{
+    backgroundColor:"yellow",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginTop: 2,
+    marginRight: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor:"lightgray",
+    resizeMode: "cover",
+  },
   container: {
-    marginTop: 25,
+    marginTop: 5,
     padding: 1,
   },
+  grpinfo:{
+    flex: 1,
+    backgroundColor:"lightskyblue",
+  },
+  name:{
+    flex: 1,
+    fontWeight:"bold",
+    fontSize: 18,
+  },
+  subtitle:{
+    color: "dimgray",
+    fontSize: 13,
+  },
+  row:{
+    flexDirection: "row",
+    marginBottom: 5,
+  },
   item: {
-    backgroundColor: "lightskyblue",
-    padding: 25,
-    marginVertical: 10,
-    marginHorizontal: 16,
+    backgroundColor:"lightgreen",
+    height: 75,
+    flexDirection: "row",
+    padding: 10,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor:"lightgray",
   },
 });
 
+export default Index;
