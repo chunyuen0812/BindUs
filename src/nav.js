f17b50d63e7dd1b165711941a89011bf370af679
Ja$on <chunyuen0812@gmail.com>
2023-02-26 22:06:05 +0800

    tabbar
    tabbar navigation added

--- src/nav.js
+++ src/nav.js
@@ -2,16 +2,39 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Login from "./pages/login";
 import Home from './pages/home';
+import Profile from './pages/profile';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator >
+      <Tab.Screen name="Home" 
+      component={Home} 
+      options={{headerShown:false,
+        tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),}}/>
+      <Tab.Screen name="Profile" 
+      component={Profile} 
+      options={{headerShown:false,
+        tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),}}/>
+    </Tab.Navigator>
+  );
+}
 
 function Nav() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName="Login">
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={MainTabs}/>
         <Stack.Screen name="Login" component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/pages/profile/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+// 這一頁可隨意修改
+class Index extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <View>
+                <Text></Text>
+                <Text>Profile Screen</Text>
+            </View>
+
+        );
+    }
+}
+ 
+export default Index;
