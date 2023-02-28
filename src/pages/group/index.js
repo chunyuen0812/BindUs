c74d52a65abcf2e68e2a8f15eee0f16eecc8ce6d
jtam2204 <jtam2204@gmail.com>
2023-02-28 23:51:02 +0800

    Create index.js

---
+++ src/pages/group/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <View>
+        <Text>index</Text>
+      </View>
+    )
+  }
+}
+
+const styles= StyleSheet.create({
+    
+})
+
+export default index
